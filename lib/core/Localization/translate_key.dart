e29b981bb800c08660686744b833052475357989
Yousef Amr <yousefamro@std.mans.edu.eg>
2025-12-26 19:39:34 +0200

    >>>

---
+++ lib/core/Localization/translate_key.dart
@@ -0,0 +1,17 @@
+class TranslateKey {
+  static String welcome = "welcome";
+  static String singInMessage = "singInMessage";
+  static String emailMessage = "emailMessage";
+  static String passWordMessage = "passWordMessage";
+  static String passWord2Message = "passWord2Message";
+  static String nameMessage = "nameMessage";
+  static String singInButton = "singInButton";
+  static String or = "or";
+  static String googleLogin = "googleLogin";
+  static String facebookLogin = "facebookLogin";
+  static String forgotPassword = "forgotPassword";
+  static String donthaveacc = "donthaveacc";
+  static String register = "register";
+  static String singUpButton = "singUpButton";
+  static String haveacc = "haveacc";
+}
